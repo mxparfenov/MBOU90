fba0904864ed4061c8e300d14b9c93fece7bc754
Maxim <08max0820@gmail.com>
2023-05-02 16:49:31 +0200

    first commit

--- script.js
+++ script.js
@@ -45,8 +45,8 @@ function rollSlider(){
 
 
 burgerIcon = document.querySelector('.wrapper .container .navigation .burger .burger_img');
-burgerMenu = document.querySelector('.burger__menu');
-burgerExit = document.querySelector('.burger__exit');
+burgerMenu = document.querySelector('.burger__menu'); // получение элемента Бургер Меню
+burgerExit = document.querySelector('.burger__exit'); // получение кнопки Выход
 
 burgerIcon.addEventListener('click', function(){
     burgerMenu.style.display = 'flex';
