8e0e649a2fa6607271a941a827494e25a103cd4e
Maxim <08max0820@gmail.com>
2023-05-10 10:55:46 +0200

    mid changes

--- JS/bezopasnost.js
+++ JS/bezopasnost.js
@@ -77,3 +77,99 @@ eventsButton.addEventListener('click', function() {
     roditelyam.style.display = 'none';
     medpage.style.display = 'none';
 });
+
+
+// Меню для экранов меньше 768 пикселей
+
+SubsectionMenu = document.querySelector('.subsection__menu');
+MenuExit = document.querySelector('.menu__exit');
+
+SubsectionMenu.addEventListener('click', function(){
+    document.querySelector('.subsection__menu__item').style.display = 'flex';
+});
+MenuExit.addEventListener('click', function(){
+    Exitmenu();
+});
+
+function Exitmenu(){
+    document.querySelector('.subsection__menu__item').style.display = 'none';
+}
+
+terrorizmButtonMenu = document.querySelector('.terrorizm-menu');
+profilaktikaButtonMenu = document.querySelector('.profilaktika-menu');
+roditelyamButtonMenu = document.querySelector('.roditelyamBut-menu');
+medpageButtonMenu = document.querySelector('.medPageBut-menu');
+helpfulVideoButtonMenu = document.querySelector('.helpfulVideoBut-menu');
+grazdOboronaButtonMenu = document.querySelector('.grazdOboronaBut-menu');
+eventsButtonMenu = document.querySelector('.eventsBut-menu');
+
+terrorizmButtonMenu.addEventListener('click', function(){
+    terrorizm.style.display = 'flex';
+    profilaktika.style.display = 'none';
+    roditelyam.style.display = 'none';
+    medpage.style.display = 'none';
+    grazdOborona.style.display = 'none';
+    helpfulVideo.style.display = 'none';
+    events.style.display = 'none';
+    Exitmenu();
+});
+profilaktikaButtonMenu.addEventListener('click', function(){
+    profilaktika.style.display = 'flex';
+    terrorizm.style.display = 'none';
+    roditelyam.style.display = 'none';
+    medpage.style.display = 'none';
+    grazdOborona.style.display = 'none';
+    helpfulVideo.style.display = 'none';
+    events.style.display = 'none';
+    Exitmenu();
+});
+roditelyamButtonMenu.addEventListener('click', function(){
+    roditelyam.style.display = 'flex';
+    terrorizm.style.display = 'none';
+    profilaktika.style.display = 'none';
+    medpage.style.display = 'none';
+    grazdOborona.style.display = 'none';
+    helpfulVideo.style.display = 'none';
+    events.style.display = 'none';
+    Exitmenu();
+});
+medpageButtonMenu.addEventListener('click', function(){
+    medpage.style.display = 'flex';
+    terrorizm.style.display = 'none';
+    profilaktika.style.display = 'none';
+    roditelyam.style.display = 'none';
+    grazdOborona.style.display = 'none';
+    helpfulVideo.style.display = 'none';
+    events.style.display = 'none';
+    Exitmenu();
+});
+helpfulVideoButtonMenu.addEventListener('click', function() {
+    helpfulVideo.style.display = 'flex';
+    terrorizm.style.display = 'none';
+    profilaktika.style.display = 'none';
+    roditelyam.style.display = 'none';
+    medpage.style.display = 'none';
+    grazdOborona.style.display = 'none';
+    events.style.display = 'none';
+    Exitmenu();
+});
+grazdOboronaButtonMenu.addEventListener('click', function() {
+    grazdOborona.style.display = 'flex';
+    helpfulVideo.style.display = 'none';
+    terrorizm.style.display = 'none';
+    profilaktika.style.display = 'none';
+    roditelyam.style.display = 'none';
+    medpage.style.display = 'none';
+    events.style.display = 'none';
+    Exitmenu();
+});
+eventsButtonMenu.addEventListener('click', function() {
+    events.style.display = 'flex';
+    grazdOborona.style.display = 'none';
+    helpfulVideo.style.display = 'none';
+    terrorizm.style.display = 'none';
+    profilaktika.style.display = 'none';
+    roditelyam.style.display = 'none';
+    medpage.style.display = 'none';
+    Exitmenu();
+});
